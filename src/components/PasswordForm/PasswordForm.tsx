aeec22f2460db9ba072b31b59e28e935e20da190
Lavergnat Jérémie <lavergnatj@outlook.fr>
2025-03-20 12:39:24 +0100

    feat: enhance password form with checkbox options and strength indicator

--- src/components/PasswordForm/PasswordForm.tsx
+++ src/components/PasswordForm/PasswordForm.tsx
@@ -1,19 +1,89 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Check } from "lucide-react";
 import Slider from "../Slider/Slider";
 import Strength from "../Strength/Strength";
 
 export default function PasswordForm() {
   return (
-    <div className="flex flex-col  bg-[var(--dark-gray)] p-4 h-126 w-full">
+    <div className="flex flex-col bg-[var(--dark-gray)] p-6 h-126 w-full justify-between">
       <Slider />
-      <ul>
-        <li>Include UpperCase Letters</li>
-        <li>Include Lowercase Letters</li>
-        <li>Includes Numbers</li>
-        <li>Includes Symbols</li>
+      <ul className="flex flex-col gap-4">
+        <ul>
+          <li className="flex items-center gap-2">
+            <label
+              htmlFor="uppercase"
+              className="relative flex items-center gap-4 cursor-pointer font-bold"
+            >
+              <input
+                type="checkbox"
+                name="uppercase"
+                id="uppercase"
+                className="w-5 h-5 appearance-none border border-[var(--green)] bg-[var(--dark-gray)] 
+                   checked:bg-[var(--green)] checked:border-[var(--green)] cursor-pointer relative"
+              />
+              <span className="absolute inset-0 left-0.5 flex items-center text-[var(--dark-gray)]">
+                <Check size={16} />
+              </span>
+              Include UpperCase Letters
+            </label>
+          </li>
+        </ul>
+        <li className="flex items-center gap-2">
+          <label
+            htmlFor="lowercase"
+            className="relative flex items-center gap-4 cursor-pointer font-bold"
+          >
+            <input
+              type="checkbox"
+              name="lowercase"
+              id="lowercase"
+              className="w-5 h-5 appearance-none border border-[var(--green)] bg-[var(--dark-gray)] 
+                   checked:bg-[var(--green)] checked:border-[var(--green)] cursor-pointer relative"
+            />
+            <span className="absolute inset-0 left-0.5 flex items-center text-[var(--dark-gray)]">
+              <Check size={16} />
+            </span>
+            Include Lowercase Letters
+          </label>
+        </li>
+        <li className="flex items-center gap-2">
+          <label
+            htmlFor="numbers"
+            className="relative flex items-center gap-4 cursor-pointer font-bold"
+          >
+            <input
+              type="checkbox"
+              name="numbers"
+              id="numbers"
+              className="w-5 h-5 appearance-none border border-[var(--green)] bg-[var(--dark-gray)] 
+                   checked:bg-[var(--green)] checked:border-[var(--green)] cursor-pointer relative"
+            />
+            <span className="absolute inset-0 left-0.5 flex items-center text-[var(--dark-gray)]">
+              <Check size={16} />
+            </span>
+            Includes Numbers
+          </label>
+        </li>
+        <li className="flex items-center gap-2">
+          <label
+            htmlFor="symbols"
+            className="relative flex items-center gap-4 cursor-pointer font-bold"
+          >
+            <input
+              type="checkbox"
+              name="symbols"
+              id="symbols"
+              className="w-5 h-5 appearance-none border border-[var(--green)] bg-[var(--dark-gray)] 
+                   checked:bg-[var(--green)] checked:border-[var(--green)] cursor-pointer relative"
+            />
+            <span className="absolute inset-0 left-0.5 flex items-center text-[var(--dark-gray)]">
+              <Check size={16} />
+            </span>
+            Includes Symbols
+          </label>
+        </li>
       </ul>
       <Strength />
-      <button className="bg-[var(--green)] text-black w-11/12 mx-auto p-4 flex justify-center items-center gap-4 font-semibold">
+      <button className="bg-[var(--green)] text-black w-full mx-auto p-4 flex justify-center items-center gap-4 font-semibold hover:text-[var(--green)] hover:bg-[var(--dark-gray)] hover:border hover:border-[var(--green)] cursor-pointer">
         GENERATE <ArrowRight />
       </button>
     </div>
--- src/components/Strength/Strength.tsx
+++ src/components/Strength/Strength.tsx
@@ -1,3 +1,40 @@
+"use client";
+
 export default function Strength() {
-  return <div>Strength</div>;
+  return (
+    <div className="bg-[var(--background)] py-4 px-6 flex items-center justify-between">
+      <p className="text-[var(--light-gray)] font-semibold">STRENGTH</p>
+      <div className="flex items-center gap-4">
+        <p className="text-lg font-bold">MEDIUM</p>
+        <div className="flex gap-1">
+          <FilledSquare str="weak" />
+          <FilledSquare str="medium" />
+          <FilledSquare str="strong" />
+          <FilledSquare />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FilledSquare({ str }: { str?: string }) {
+  if (!str) {
+    return <div className="w-2 h-6 border border-foreground"></div>;
+  }
+  function getSquareBg(str: string): string {
+    switch (str) {
+      case "tooweak":
+        return "bg-[var(--red)]";
+      case "weak":
+        return "bg-[var(--orange)]";
+      case "medium":
+        return "bg-[var(--yellow)]";
+      case "strong":
+        return "bg-[var(--green)]";
+      default:
+        return "bg-gray-500";
+    }
+  }
+
+  return <div className={`w-2 h-6 ${getSquareBg(str)}`}></div>;
 }
